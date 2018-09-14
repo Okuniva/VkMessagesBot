0624cbb81dcfafe24e818dfa3a9bae143f0417a3
alexander.valeev <alexander.valeev@binwell.com>
2018-09-14 12:49:11 -0700

    Add and init TelegramBot

--- VkMessagesBot/Program.cs
+++ VkMessagesBot/Program.cs
@@ -1,16 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 
 namespace VkMessagesBot
 {
-	class Program
+	static class Program
 	{
+		private static readonly HttpClient Client = new HttpClient();
+
+		private static readonly TelegramBotClient Bot = new TelegramBotClient(Consts.TelegramBotAccessToken);
+
 		static void Main(string[] args)
 		{
-			
+			var me = Bot.GetMeAsync().Result;
+			Console.Title = me.Username;
+
+			Bot.StartReceiving(Array.Empty<UpdateType>());
+			Console.WriteLine($"Start listening for @{me.Username}");
+			Console.ReadLine();
+			Console.ReadLine();
+			Bot.StopReceiving();
 		}
 	}
 }
